0e416a8518aed93baf15fba2965a03e5dda6fe5c
KumundzhievMaxim <kumundzhievmaxim@gmail.com>
2021-04-17 22:01:09 +0200

    Added inference script && configuration.

---
+++ configurations.py
@@ -0,0 +1,14 @@
+# ------------------------------------------
+# 
+# Program created by Maksim Kumundzhiev
+#
+#
+# email: kumundzhievmaxim@gmail.com
+# github: https://github.com/KumundzhievMaxim
+# -------------------------------------------
+
+
+BATCH_SIZE = 10
+IMG_SIZE = (160, 160)
+
+MODEL_PATH = '/tmp/model'
---
+++ inference.py
@@ -0,0 +1,54 @@
+import argparse
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+import keras
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+
+
+from configurations import IMG_SIZE, BATCH_SIZE, MODEL_PATH
+
+
+class Inference:
+    def prepare_dataset(self, dataset_path):
+        test_dataset = tf.keras.preprocessing.image_dataset_from_directory (
+            dataset_path,
+            shuffle=True,
+            seed=123456,
+            image_size=IMG_SIZE,
+            batch_size=BATCH_SIZE)
+        return test_dataset
+
+    @staticmethod
+    def load_model():
+        return keras.models.load_model(MODEL_PATH)
+
+    def run(self, path: str):
+        test_dataset = self.prepare_dataset(dataset_path=path)
+        inference_model = self.load_model()
+
+        image_batch, label_batch = test_dataset.as_numpy_iterator().next()
+        predictions = inference_model.predict_on_batch(image_batch).flatten()
+        predictions = tf.nn.sigmoid(predictions)
+        predictions = tf.where(predictions < 0.5, 0, 1)
+
+        for image_class in predictions:
+            if image_class == 1:
+                pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path', required=True, type=str,
+                        help='Path to the dataset.')
+    arguments = parser.parse_args()
+
+    Inference.run(path=arguments.path)
+
+
+
+
